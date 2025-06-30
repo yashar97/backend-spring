7986c0b326c1029c003f918093a3f27bcb288740
yashar <yasharg97@gmail.com>
2025-06-30 18:18:53 -0500

    primer commit

---
+++ src/main/java/com/clientes_backend/ClientesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clientes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clientes_backend/ClientesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
